291ed0cbd62a1e625cbe93a84fe5b3c632a531f7
Robin Aldenhoven <robin.aldenhoven@gmail.com>
2013-11-22 22:01:31 +0100

    Update cgminer.rb
    Forgot to update hash

--- cgminer.rb
+++ cgminer.rb
@@ -4,7 +4,7 @@ class Cgminer < Formula
   homepage 'https://github.com/ckolivas/cgminer'
   head 'https://github.com/ckolivas/cgminer.git', :branch => 'master'
   url 'https://github.com/ckolivas/cgminer/archive/v3.7.0.tar.gz'
-  sha1 'bb67a403b1234679cc71192507b086eb2bb60147'
+  sha1 'a70e273edd90d531c829e2dca7a4d62856a2da37'
 
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
